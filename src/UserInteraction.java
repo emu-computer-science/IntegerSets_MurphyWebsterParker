ac3877962d2f0416f5f96183ba922fedd9b86776
darkpark02 <jparke62@emich.edu>
2025-07-12 22:21:17 -0400

    Park did his job

--- src/UserInteraction.java
+++ src/UserInteraction.java
@@ -1,4 +1,45 @@
 
 public class UserInteraction {
 
+	
+	public UserInteraction() {
+		
+	}
+	
+	private void handleNew() {
+		
+	}
+	private void handleAdd() {
+		
+	}
+	private void handleShow() {
+		
+	}
+	private void handleReverse() {
+		
+	}
+	private void handleSelect() {
+		
+	}
+	private void handleDelete() {
+		
+	}
+	private void handleSort() {
+		
+	}
+	private void handleRandomize() {
+		
+	}
+	private void handleSave() {
+
+	}
+	private void handleRestore() {
+		
+}
+	private void handleHelp() {
+		
+	}
+	private void handleQuit() {
+		
+	}
 }
