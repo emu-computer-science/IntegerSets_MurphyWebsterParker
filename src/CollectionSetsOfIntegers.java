cbe9b898103a4951301539a1f62bd43a6d691f64
Devin Webster <websterdevinr@hotmail.com>
2025-07-12 21:08:23 -0400

    Added declaration to CollectionSetOfIntegers - DW

--- src/CollectionSetsOfIntegers.java
+++ src/CollectionSetsOfIntegers.java
@@ -1,4 +1,24 @@
 
 public class CollectionSetsOfIntegers {
-
+	public void listAll() {
+		System.out.println("You have called the listAll function!");
+	}
+	
+	public void createSet() {
+		System.out.println("You have called the createNewSet function!");
+	}
+	
+	public void deleteSet() {
+		System.out.println("You have called the deleteSet function!");
+	}
+	
+	public void restore() {
+		System.out.println("You have called the restore function!");
+	}
+	
+	public static Integer[][] save() {
+		System.out.println("You have called the save function!");
+		Integer[][] fuck = {{}};
+		return fuck;
+	}
 }
