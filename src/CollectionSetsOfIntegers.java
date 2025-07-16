fa702752695d53b0a2ef3a2db7323174018c75e4
Devin Webster <websterdevinr@hotmail.com>
2025-07-15 21:30:35 -0400

    progressed on listAll command in CollectionSetOfIntegers - DW

--- src/CollectionSetsOfIntegers.java
+++ src/CollectionSetsOfIntegers.java
@@ -1,24 +1,67 @@
+//Created by Dibblz
+//7/14/25
+//COSC 381
 
+
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+/* */
 public class CollectionSetsOfIntegers {
-	public void listAll() {
-		System.out.println("You have called the listAll function!");
-	}
+	static /*variable for the array of int arrays */
+	ArrayList<int[]> sets = new ArrayList<>();
 	
-	public void createSet() {
+	
+	/* Lists all the sets(int arrays) in the list of sets(array of int array)
+	 * */	
+	public static void listAll(int[][] x) {
+		System.out.println("You have called the listAll function!");
+		for(int i = 0; i  < x.length; i++) {
+			char letter = (char) ('A'+i);
+				System.out.println(letter + ":" + Arrays.toString(x[i]));
+			}
+			
+		}
+	
+	/* Creates an array and adds it to the array  of arrays
+	 * */	
+	public static void createSet(int[] x) {
 		System.out.println("You have called the createNewSet function!");
 	}
 	
+	
+	/* Deletes an array from the array  of arrays
+	 * */
 	public void deleteSet() {
 		System.out.println("You have called the deleteSet function!");
 	}
 	
+	
+	/* Restores a save file, saved using CollectionSetsOfIntegers.save().
+	 * This will restore the saved array  of arrays	 * */
 	public void restore() {
 		System.out.println("You have called the restore function!");
 	}
 	
+	
+	/* Save function that stores your array of arrays into a text file. 
+	 * */
 	public static Integer[][] save() {
 		System.out.println("You have called the save function!");
 		Integer[][] sets = {{}};
 		return sets;
 	}
+	
+	/*public static void main(String[] args) {
+		int[][] array1 = {
+				{1,2,3,4},
+				{1},
+				{1,2,3,4,5},
+				{00000000000}}; */
+	
+	//	listAll(array1);
+	//}
+		
 }
+
